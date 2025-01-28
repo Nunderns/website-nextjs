062444a5dc3cfb0bb98a36c2c917464214a91c3e
Henri Okayama <67149267+Nunderns@users.noreply.github.com>
2025-01-28 00:38:55 -0300

    feat: sheet aplication

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,26 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-            <ThemeProvider
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-            >
-            {children}
+          >
+            <div className="min-h-screen">
+              <Navbar />
+              <main className="py-8">
+                {/* Container to centralizar o conteúdo */}
+                <div className="max-w-7xl mx-auto px-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                    {/* Espaço vazio para layout futuro */}
+                    <div className="hidden lg:block lg:col-span-3"></div>
+                    {/* Conteúdo principal */}
+                    <div className="lg:col-span-9">{children}</div>
+                  </div>
+                </div>
+              </main>
+            </div>
           </ThemeProvider>
         </body>
       </html>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+// src/components/Navbar.js ou Navbar.tsx
+export default function Navbar() {
+    return <div>Navbar Content</div>;
+  }
+  
