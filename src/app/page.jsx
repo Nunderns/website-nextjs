75e169e633bbb6bda7488bdcbbb820f6c3557921
henri <henri.okayama@gmail.com>
2025-01-28 10:34:15 -0300

    fixed: navbar desktop

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,23 +1,10 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Logar</Button>
-        </SignInButton>
-      </SignedOut>
-      
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>Logar</Button>
-
-      <ModeToggle/>
+      <h1>Página principal</h1>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,24 @@
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+
   return (
-    <div className="sticky top-0 w-full border-b bg-background/95 backdrop-blur-sm supports-[backdrop-filter]:bg-background/60 z-50">
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center space-x-4">
-            {/* Logo */}
-            <Link
-              href="/"
-              className="text-xl font-bold text-primary font-mono tracking-wider"
-            >
+          <div className="flex items-center">
+            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
               Socially
             </Link>
-  
-            {/* Desktop Navbar */}
-            <DesktopNavbar />
-            {/* Uncomment the Mobile Navbar if needed */}
-            {/* <MobileNavbar /> */}
           </div>
+
+          <DesktopNavbar />
         </div>
       </div>
-    </div>
+    </nav>
   );
-  
-  }
-  
+}
+export default Navbar;
