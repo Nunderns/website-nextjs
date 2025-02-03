c8f002692b1c7043c2d7eb83d3423ab0ad5ed7cb
henri <henri.okayama@gmail.com>
2025-02-03 13:03:16 -0300

    feat: like, comment delete posts

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import { getPosts } from "@/actions/post.action";
 import { getDbUserId } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
 import { currentUser } from "@clerk/nextjs/server";
+import {PostCard} from "@/components/PostCard"
 
 export default async function Home() {
   const user = await currentUser();
@@ -12,6 +13,12 @@ export default async function Home() {
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
         {user ? <CreatePost /> : null}
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
       </div>
     </div>
   );
